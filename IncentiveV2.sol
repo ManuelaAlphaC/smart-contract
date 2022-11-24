5bec22dbbb9284f85f7d6458566c3550174bb20b
manuelita <97229784+ManuelaAlphaC@users.noreply.github.com>
2022-11-24 21:43:55 +0100

    Update IncentiveV2.sol

--- IncentiveV2.sol
+++ IncentiveV2.sol
@@ -14,10 +14,11 @@ contract incentiveV1 is ERC721, ERC721Enumerable, ERC721URIStorage {
 
     mapping(address => SellerAccount[]) public _sellers;
     mapping(address => BuyerAccount[]) public _buyers;
-    mapping(uint256 => Incentive[]) public _incentives;
+    mapping(uint256 => specialIncentive[]) public _incentives;
 
     mapping(address => bool) public _onlySellers;
 
+/************************************* EVENTS **********************************/
     event WasCreatedSellerAccount(
         uint256 indexed IdAccount,
         string name,
@@ -34,7 +35,7 @@ contract incentiveV1 is ERC721, ERC721Enumerable, ERC721URIStorage {
         address indexed buyer
     );
 
-    event WasCreatedIncentive(
+    event WasCreatedSpecialIncentive(
         uint256 indexed IdIncentive,
         address indexed creator,
         string name,
@@ -46,6 +47,25 @@ contract incentiveV1 is ERC721, ERC721Enumerable, ERC721URIStorage {
         uint256 save
     );
 
+    event WasCreatedIncentive(
+        uint256 indexed IdIncentive,
+        address indexed creator,
+        string name,
+        string description,
+        string incentiveURI,
+        uint256 price
+    );
+ /************************************** STRUCTS *******************************************/
+
+ /*
+   * @notice i dati necessari per creare un'account seller
+   *
+   * @param name --> Il nome utente
+   * @param profileImage --> Immagine profilo dell'utente
+   * @param bio --> Una breve descrizione sul seller
+   * @param seller --> L'address del proprietario di questo profilo, coincide con il mitente 
+   * @param IdAccount --> L'id dell'account appena creato, viene impostato automaticamente
+ */ 
     struct SellerAccount{
         string name;
         string profileImage;
@@ -54,6 +74,15 @@ contract incentiveV1 is ERC721, ERC721Enumerable, ERC721URIStorage {
         uint256 IdAccount;
     }
 
+ /*
+   * @notice i dati necessari per creare un'account buyer
+   *
+   * @param name --> Il nome utente
+   * @param profileImage --> Immagine profilo dell'utente
+   * @param preferences --> Le preferenze dell'utente (cucina vegana, mediterranea, cinese)
+   * @param buyer --> L'address del proprietario di questo profilo, coincide con il mitente 
+   * @param IdAccount --> L'id dell'account appena creato, viene impostato automaticamente
+ */ 
     struct BuyerAccount{
         string name;
         string profileImage;
@@ -62,21 +91,53 @@ contract incentiveV1 is ERC721, ERC721Enumerable, ERC721URIStorage {
         uint256 IdAccount;
     }
 
-    struct Incentive{
+ /*
+   * @notice incentivo al quale si applica uno sconto
+   *
+   * @param name --> Il nome del piato
+   * @param description --> descrizione del piato
+   * @param incentiveURI --> Immagine/video che rappresenta il piato
+   * @param price --> Il prezzo iniziale del piato
+   * @param discount --> Lo sconto da applicare al piato, senza percentuale 20 = 20%
+   * @param IdIncentive --> L'id dell'incentivo appena creato, viene impostato automaticamente
+   * param creator --> L'address del account seller e deve coincidere con il mitente
+ */ 
+    struct specialIncentive{
         string name;
         string description;
         string incentiveURI;
         uint256 price;
-        uint256 discount; // numero senza percentuale 20 = 20%
+        uint256 discount;
         uint256 IdIncentive;
         address creator;
     }
 
+/*
+   * @notice incentivo con prezzo fisso
+   *
+   * @param name --> Il nome del piato
+   * @param description --> descrizione del piato
+   * @param incentiveURI --> Immagine/video che rappresenta il piato
+   * @param price --> Il prezzo fisso del piato
+   * @param IdIncentive --> L'id dell'incentivo appena creato, viene impostato automaticamente
+   * param creator --> L'address del account seller e deve coincidere con il mitente
+ */
+    struct Incentive {
+        string name;
+        string description;
+        string incentiveURI;
+        uint256 price;
+        uint256 IdIncentive;
+        address creator;
+    }
+
+/******************************** ARRAYS *************************************/
     SellerAccount[] public sellers;
     BuyerAccount[] public buyers;
+    specialIncentive[] public specialIncentives;
     Incentive[] public incentives;
 
-    constructor() ERC721("Incentive", "V1"){}
+    constructor() ERC721("Incentive", "V2"){}
 
     modifier onlySellers {
         require(_onlySellers[msg.sender]);
@@ -107,7 +168,7 @@ contract incentiveV1 is ERC721, ERC721Enumerable, ERC721URIStorage {
     }
 
  /******************* INCENTIVE *************************/
-    function createIncentive(
+    function createSpecialIncentive(
         string memory name,
         string memory description,
         string memory incentiveURI,
@@ -118,7 +179,33 @@ contract incentiveV1 is ERC721, ERC721Enumerable, ERC721URIStorage {
         uint256 IdIncentive = _tokenIdCounter.current();
         uint256 newprice = price - (price * (discount/100));
         uint256 save = price * (discount/100);
-        incentives.push(Incentive(name, description, incentiveURI, price, discount, IdIncentive, creator));
+        specialIncentives.push(specialIncentive(name, description, incentiveURI, price, discount, IdIncentive, creator));
+        _tokenIdCounter.increment();
+        _mint(msg.sender, IdIncentive);
+        _setTokenURI(IdIncentive, incentiveURI);
+
+        emit WasCreatedSpecialIncentive(
+            IdIncentive,
+            creator,
+            name,
+            description,
+            incentiveURI,
+            price,
+            discount,
+            newprice,
+            save 
+        );
+    }
+
+    function createIncentive(
+        string memory name,
+        string memory description,
+        string memory incentiveURI,
+        uint256 price,
+        address creator
+    ) public onlySellers {
+        uint256 IdIncentive = _tokenIdCounter.current();
+        incentives.push(Incentive(name, description, incentiveURI, price, IdIncentive, creator));
         _tokenIdCounter.increment();
         _mint(msg.sender, IdIncentive);
         _setTokenURI(IdIncentive, incentiveURI);
@@ -129,10 +216,7 @@ contract incentiveV1 is ERC721, ERC721Enumerable, ERC721URIStorage {
             name,
             description,
             incentiveURI,
-            price,
-            discount,
-            newprice,
-            save 
+            price
         );
     }
 
@@ -167,13 +251,18 @@ contract incentiveV1 is ERC721, ERC721Enumerable, ERC721URIStorage {
         return buyers;
     }
 
-    function getIncentives() public view returns(Incentive[] memory) {
-        return incentives;
+    function getIncentives() public view returns(specialIncentive[] memory) {
+        return specialIncentives;
     }
  /************************************************************************/
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable){
-        super._beforeTokenTransfer(from, to, tokenId);
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal override (ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
     }
 
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
